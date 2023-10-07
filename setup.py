fab7216e3a1720a438c5010ac741b63e415b251d
ChrisWest42 <chris.westersund@gmail.com>
2023-10-07 17:03:47 +0000

    push from PythonAnywhere

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of everylotbot
+
+from setuptools import setup
+
+with open('everylot/__init__.py') as i:
+    version = next(r for r in i.readlines() if '__version__' in r).split('=')[1].strip('"\' \n')
+
+setup(
+    name='everylot',
+    version=version,
+    description='everylot',
+    long_description='''every lot''',
+    keywords='',
+    author='fitnr',
+    author_email='fitnr@fakeisthenewreal',
+    packages=['everylot'],
+    license='GPL-3.0',
+    include_package_data=False,
+    install_requires=[
+    'twitter_bot_utils>=0.11.5,<=0.14', # Updated to 0.14
+    'tweepy>=4.0.0'
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'everylot=everylot.bot:main',
+        ],
+    },
+)
